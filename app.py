7f9b55dc2641b6845ecf9a58eea8a573f0efa616
Irsal Pebri <133020360+ceritaceritanabi@users.noreply.github.com>
2023-05-17 11:15:41 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit
+import pycaret
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('y2.csv')
+x = dataset.iloc[:, 0:2].values
+y = dataset.iloc[:, -1].values
+dataset.head()
+
+
+from pycaret.regression import *
+s = setup(dataset, target = 'y2', session_id=123,
+         normalize = True,
+           polynomial_features = True, trigonometry_features = True,
+           feature_interaction=True, 
+           bin_numeric_features= ['C-axis rear bearing', 'A-axis rear bearing'])
+
+
+best = compare_models()
+
+
+br_model = create_model('br')
+evaluate_model(br_model)
+predict_model(br_model)
+
+save_model(br_model, model_name = 'Bayesian Ridge')
+
+from pycaret.regression import load_model, predict_model
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+def predict_quality(model, dataset):
+    
+    predictions_data = predict_model(estimator = model, data = dataset)
+    return predictions_data['Label'][0]
+    
+model = load_model('Bayesian Ridge')
+
+
+st.title('Thermal Deformation Prediction')
+st.write('This is a web app to predict the thermal deformation based on\
+         several features that you can see in the sidebar. Please adjust the\
+         value of each feature. After that, click on the Predict button at the bottom to\
+         see the prediction.')
+
+C_axis_rear_bearing = st.number_input(label = 'C-axis_rear_bearing', min_value = 0.0,
+                          max_value = 100.00 ,
+                          value = 20.0,
+                          step = 0.1)
+
+A_axis_rear_bearing = st.number_input(label = 'A-axis rear bearing', min_value = 0.0,
+                          max_value = 100.00 ,
+                          value = 20.0,
+                          step = 1.0)
+
+features = {'C-axis rear bearing': C_axis_rear_bearing, 'A-axis rear bearing': A_axis_rear_bearing
+        
+            }
+ 
+
+features_df  = pd.DataFrame([features])
+
+st.table(features_df)  
+
+if st.button('Predict'):
+    
+    prediction = predict_quality(model, features_df)
+    
+    st.write(' Based on feature values, the thermal deformation is '+ str(prediction))
